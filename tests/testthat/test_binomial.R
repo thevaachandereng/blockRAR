ec4f71f4f80bce489b1f32fbf79fcb172e55aac7
Thevaa Chandereng <chandereng@wisc.edu>
2018-12-28 01:03:57 -0600

    added more test cases

---
+++ tests/testthat/test_binomial.R
@@ -0,0 +1,19 @@
+context("binomial RAR")
+test_that("the binomial RAR output is", {
+  set.seed(100211)
+  expect_equal(max(binomialRAR(p_control = 0.7, p_treatment = 0.7, N_total = 200,
+                           block_number = 2, simulation = 10)$N_enrolled), 200)
+  expect_equal(binomialRAR(p_control = 0.1, p_treatment = 0.5, N_total = 200,
+                               block_number = 3, simulation = 10)$power, 0)
+  expect_error(binomialRAR(p_control = 1.1, p_treatment = 0.5, N_total = 200,
+                           block_number = 3, simulation = 10))
+  expect_error(binomialRAR(p_control = 0.1, p_treatment = 1.2, N_total = 200,
+                           block_number = 3, simulation = 10))
+  expect_error(binomialRAR(p_control = 0.1, p_treatment = 1.2, N_total = -100,
+                           block_number = 3, simulation = 10))
+  expect_error(binomialRAR(p_control = 0.1, p_treatment = 0.2, N_total = -100,
+                           block_number = 1.2))
+  expect_error(binomialRAR(p_control = 0.1, p_treatment = 0.2, N_total = 100,
+                           alternative = "two-sided"))
+
+})

8a5a9f19aed7a5247376fea563ac2357a13d7db6
Thevaa Chandereng <chandereng@wisc.edu>
2019-03-13 18:16:04 -0500

    added importFrom

--- R/binomialbayes.R
+++ R/binomialbayes.R
@@ -36,7 +36,7 @@
 #'     each simulation.}
 #' }
 #'
-#' @importFrom stats rbinom binomial coef
+#' @importFrom stats rbinom binomial coef quasi
 #' @importFrom arm bayesglm sim
 #' @importFrom bayesDP bdpbinomial
 #' @importFrom dplyr mutate group_by summarize

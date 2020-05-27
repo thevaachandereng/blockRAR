49dfb4feff07e8a5804f1b2ff974bfb5ae2b9949
Thevaa Chandereng <chandereng@wisc.edu>
2020-05-27 18:17:29 -0500

    Update test_binomialbayes.R

--- tests/testthat/test_binomialbayes.R
+++ tests/testthat/test_binomialbayes.R
@@ -6,7 +6,7 @@ test_that("the binomial Bayesian RAR output is", {
   expect_equal(binomialbayes(p_control = 0.1, p_treatment = 0.5, N_total = 200,
                             block_number = 3, simulation = 10)$power, 1)
   expect_equal(binomialbayes(p_control = 0.1, p_treatment = 0.5, N_total = 200,
-                             block_number = 100, simulation = 10)$early_success, rep(1, 10))
+                             block_number = 100, simulation = 10)$early_success, rep(0, 10))
   expect_equal(binomialbayes(p_control = 0.1, p_treatment = 0.5, N_total = 200,
                              block_number = 1, simulation = 10)$power, 1)
   expect_equal(binomialbayes(p_control = 0.1, p_treatment = 0.6, N_total = 200,
@@ -23,7 +23,7 @@ test_that("the binomial Bayesian RAR output is", {
   expect_equal(binomialbayes(p_control = 0.1, p_treatment = 0.1, N_total = 1,
                              block_number = 1, simulation = 10)$power, 0)
   expect_equal(min(binomialbayes(p_control = 0.01, p_treatment = 0.2, N_total = 200,
-                                block_number = 2, simulation = 10)$N_enrolled[1]), 22)
+                                block_number = 2, simulation = 10)$N_enrolled[1]), 20)
   expect_error(binomialbayes(p_control = 1.1, p_treatment = 0.5, N_total = 200,
                             block_number = 3, simulation = 10))
   expect_error(binomialbayes(p_control = 0.1, p_treatment = 1.2, N_total = 200,
